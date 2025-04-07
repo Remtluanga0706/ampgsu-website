6867cdaea5db7503b4394ad5ea09c1be49027b46
Remtluanga0706 <remtluanga07062003@gmail.com>
2025-04-07 11:38:41 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Optional: connect to Google Sheets using Tabletop.js
+// Include Tabletop via CDN in <head> if used
+
+// const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/pubhtml';
+
+// function init() {
+//   Tabletop.init({
+//     key: publicSpreadsheetUrl,
+//     callback: showInfo,
+//     simpleSheet: true,
+//   });
+// }
+
+// function showInfo(data, tabletop) {
+//   console.log(data);
+//   // render data to sections
+// }
+
+// window.addEventListener('DOMContentLoaded', init);
