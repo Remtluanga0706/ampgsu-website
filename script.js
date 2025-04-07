3cd787731a779e852c97ec92f8249f2e30e39a2c
Remtluanga0706 <remtluanga07062003@gmail.com>
2025-04-07 11:43:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-// Optional: connect to Google Sheets using Tabletop.js
-// Include Tabletop via CDN in <head> if used
+const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/pubhtml';
 
-// const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/pubhtml';
+function init() {
+  Tabletop.init({
+    key: publicSpreadsheetUrl,
+    callback: showData,
+    simpleSheet: false
+  });
+}
 
-// function init() {
-//   Tabletop.init({
-//     key: publicSpreadsheetUrl,
-//     callback: showInfo,
-//     simpleSheet: true,
-//   });
-// }
+function showData(data) {
+  const officeBearers = data["office_bearers"].elements;
+  const notifications = data["notifications"].elements;
+  const achievements = data["achievements"].elements;
 
-// function showInfo(data, tabletop) {
-//   console.log(data);
-//   // render data to sections
-// }
+  let obHTML = "";
+  officeBearers.forEach((item) => {
+    obHTML += `<div><h3>${item.Name} - ${item.Designation}</h3><p>Year: ${item.Year}</p></div>`;
+  });
+  document.getElementById("officeBearers").innerHTML = obHTML;
 
-// window.addEventListener('DOMContentLoaded', init);
+  let notiHTML = "";
+  notifications.forEach((item) => {
+    notiHTML += `<p>${item.Date}: ${item.Title}</p>`;
+  });
+  document.getElementById("notificationsList").innerHTML = notiHTML;
+
+  let achHTML = "";
+  achievements.forEach((item) => {
+    achHTML += `<p>${item.Name} - ${item.Achievement}</p>`;
+  });
+  document.getElementById("achievementsList").innerHTML = achHTML;
+}
+
+window.addEventListener("DOMContentLoaded", init);
